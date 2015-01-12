140fbdd36c018d3bc444dcd19eed9d0fb571f6c0
Joey Novak <joey.novak@gmail.com>
2015-01-12 15:57:00 -0700

    Added play with pause to options.

--- play_epic.php
+++ play_epic.php
@@ -19,11 +19,23 @@ $commands = array(
         'input touchscreen tap 1200 1100'
     ),
 
+    'pause' => array(
+        'input touchscreen tap 50 50'
+    ),
+    'replayFromPause' => array(
+        'input touchscreen tap 50 300'
+    )
 
 );
 if (!empty($_GET['action'])) {
     if(isset($commands[$_GET['action']])){
-        $cmd = '"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb" shell ' . implode(";", $commands[$_GET['action']]) . ";";
+        if(file_exists("C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe")){
+            $cmd = '"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb" ';
+        }
+        else {
+            $cmd = '"C:\Users\joey\AppData\Local\Android\sdk\platform-tools\adb" ';
+        }
+        $cmd .= ' shell ' . implode(";", $commands[$_GET['action']]) . ";";
         $out = system($cmd);
         var_dump($out);
     }
@@ -98,9 +110,30 @@ if (!empty($_GET['action'])) {
             jQuery('[value=accept]').click();
             setTimeout(make, 1000);
         }
+
+
+        function autoPlayThenPause(){
+            jQuery('body').append("Auto Playing<br/>");
+            jQuery('[value=autoPlay]').click();
+            setTimeout(pauseThenReplay, 40000);
+        }
+
+        function pauseThenReplay(){
+            jQuery('body').append("&nbsp;Pausing<br/>");
+            jQuery('[value=pause]').click();
+            setTimeout(replayFromPause, 1000);
+        }
+
+        function replayFromPause(){
+            jQuery('body').append("&nbsp;Replaying<br/>");
+            jQuery('[value=replayFromPause]').click();
+            setTimeout(autoPlayThenPause, 11000);
+        }
+
     </script>
     <input type="button" value="Auto Play" onClick="autoPlay(); return false;"/>
     <input type="button" value="Auto Make" onClick="make(); return false;"/>
+    <input type="button" value="Auto Play With Pause" onClick="autoPlayThenPause(); return false;"/>
     <form method="get">
         <input type="text" name="action" value="">
         <br/>
